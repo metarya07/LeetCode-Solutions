c610fa47ae04ae26ea494352b0711c6314a67209
metarya07 <metaryajain07@gmail.com>
2026-08-02 03:54:00 +0000

    [LeetCode #877] Stone Game - Solution 1 (Java)

---
+++ problems/0877-stone-game/README.md
@@ -0,0 +1,10 @@
+# 877. Stone Game
+
+**Difficulty:** 🟡 **Medium**  
+**Problem Link:** [Stone Game](https://leetcode.com/problems/stone-game/)
+
+## Solutions
+
+| # | File | Language | Runtime | Memory | Submitted At |
+| :--- | :--- | :--- | :--- | :--- | :--- |
+| 1 | [`solution_1.java`](./solution_1.java) | Java | 0 | 43340000 | 2026-08-02 03:54 |
---
+++ problems/0877-stone-game/solution_1.java
@@ -0,0 +1,16 @@
+/*
+ * LeetCode Problem #877: Stone Game
+ * URL: https://leetcode.com/problems/stone-game/
+ * Solution #1 (Java)
+ * Status: Accepted
+ * Runtime: 0
+ * Memory: 43340000
+ * Submission Date: 2026-08-02 03:54:01 UTC
+ * Submission ID: 2090826412
+ */
+
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
