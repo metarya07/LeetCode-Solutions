c7377842d8674cff915ca5d930c21bc49f2801cd
metarya07 <metaryajain07@gmail.com>
2026-08-03 02:00:38 +0000

    [LeetCode #1140] Stone Game II - Solution 1 (Java)

---
+++ problems/1140-stone-game-ii/README.md
@@ -0,0 +1,10 @@
+# 1140. Stone Game II
+
+**Difficulty:** 🟡 **Medium**  
+**Problem Link:** [Stone Game II](https://leetcode.com/problems/stone-game-ii/)
+
+## Solutions
+
+| # | File | Language | Runtime | Memory | Submitted At |
+| :--- | :--- | :--- | :--- | :--- | :--- |
+| 1 | [`solution_1.java`](./solution_1.java) | Java | 3 | 43952000 | 2026-08-03 02:00 |
---
+++ problems/1140-stone-game-ii/solution_1.java
@@ -0,0 +1,38 @@
+/*
+ * LeetCode Problem #1140: Stone Game II
+ * URL: https://leetcode.com/problems/stone-game-ii/
+ * Solution #1 (Java)
+ * Status: Accepted
+ * Runtime: 3
+ * Memory: 43952000
+ * Submission Date: 2026-08-03 02:00:39 UTC
+ * Submission ID: 2091978200
+ */
+
+class Solution {
+    public int stoneGameII(int[] piles) {
+        int n = piles.length;
+        int[][] dp = new int[n][n+1];
+        int[] suffixSum = new int[n];
+        suffixSum[n-1] = piles[n-1];
+        for (int i = n-2; i >= 0; i--) {
+            suffixSum[i] = piles[i] + suffixSum[i+1];
+        }
+        
+        return dfs(piles, dp, suffixSum, 0, 1);
+    }
+    
+    private int dfs(int[] piles, int[][] dp, int[] suffixSum, int i, int M) {
+        if (i == piles.length) return 0;
+        if (2 * M >= piles.length - i) return suffixSum[i];
+        if (dp[i][M] != 0) return dp[i][M];
+        
+        int min = Integer.MAX_VALUE;
+        for (int x = 1; x <= 2 * M; x++) {
+            min = Math.min(min, dfs(piles, dp, suffixSum, i + x, Math.max(M, x)));
+        }
+        
+        dp[i][M] = suffixSum[i] - min;
+        return dp[i][M];
+    }
+}
