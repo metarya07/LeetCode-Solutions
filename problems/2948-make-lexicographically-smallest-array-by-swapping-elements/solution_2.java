9c4b5fecbdee9ad1fc3b6daf01519b0d282930b5
metarya <metaryajain07@gmail.com>
2026-08-29 09:10:07 +0530

    [LeetCode #2948] Make Lexicographically Smallest Array by Swapping Elements - Solution 2 (Java)

---
+++ problems/2948-make-lexicographically-smallest-array-by-swapping-elements/solution_2.java
@@ -0,0 +1,59 @@
+/*
+ * LeetCode Problem #2948: Make Lexicographically Smallest Array by Swapping Elements
+ * URL: https://leetcode.com/problems/make-lexicographically-smallest-array-by-swapping-elements/
+ * Solution #2 (Java)
+ * Status: Accepted
+ * Runtime: 50
+ * Memory: 141196000
+ * Submission Date: 2026-08-29 03:40:09 UTC
+ * Submission ID: 2123408307
+ */
+
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+        int n = nums.length;
+        long[] sorted = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            sorted[i] = ((long) nums[i] << 32) | (i & 0xffffffffL);
+        }
+
+        Arrays.sort(sorted);
+
+        int[] values = new int[n];
+        for (int i = 0; i < n; i++) {
+            values[i] = (int) (sorted[i] >>> 32);
+        }
+
+        int start = 0;
+
+        while (start < n) {
+            int end = start;
+
+            while (end + 1 < n &&
+                   values[end + 1] - values[end] <= limit) {
+                end++;
+            }
+
+            int size = end - start + 1;
+            int[] indices = new int[size];
+
+            for (int i = 0; i < size; i++) {
+                indices[i] = (int) sorted[start + i];
+            }
+
+            Arrays.sort(indices);
+
+            for (int i = 0; i < size; i++) {
+                nums[indices[i]] = values[start + i];
+            }
+
+            start = end + 1;
+        }
+
+        return nums;
+    }
+}
