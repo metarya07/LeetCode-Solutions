e912dccc084ab5ecb1621535b60158e76d55891b
metarya <metaryajain07@gmail.com>
2026-08-31 08:01:52 +0530

    [LeetCode #2058] Find the Minimum and Maximum Number of Nodes Between Critical Points - Solution 2 (Java)

---
+++ problems/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution_2.java
@@ -0,0 +1,85 @@
+/*
+ * LeetCode Problem #2058: Find the Minimum and Maximum Number of Nodes Between Critical Points
+ * URL: https://leetcode.com/problems/find-the-minimum-and-maximum-number-of-nodes-between-critical-points/
+ * Solution #2 (Java)
+ * Status: Accepted
+ * Runtime: 4
+ * Memory: 106056000
+ * Submission Date: 2026-08-31 02:31:52 UTC
+ * Submission ID: 2125591701
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+        
+        int prev=head.val;
+        int curr,next;
+        ListNode temp=head.next;
+
+        int mindis;
+        int maxdis;
+
+        int firstidx=0,curridx=0,previdx=0;
+
+        
+
+        while(temp.next!=null){
+            curr=temp.val;
+            next=temp.next.val;
+            temp=temp.next;
+            if(curr<prev && curr<next || curr>prev && curr>next ) {
+                curridx=1;
+                firstidx=1;
+                prev=curr;
+                break;
+            }
+            prev=curr;
+        }
+
+        if(firstidx==0) return new int[]{-1,-1};
+
+        while(temp.next!=null){
+            curr=temp.val;
+            next=temp.next.val;
+            temp=temp.next;
+            curridx++;
+            if(curr<prev && curr<next || curr>prev && curr>next ) {
+                previdx=curridx;
+                prev=curr;
+                break;
+            }
+            prev=curr;
+        }
+
+        if(previdx==0) return new int[]{-1,-1};
+
+        mindis=maxdis=curridx-firstidx;
+
+           while(temp.next!=null){
+            curr=temp.val;
+            next=temp.next.val;
+            temp=temp.next;
+            curridx++;
+            if(curr<prev && curr<next || curr>prev && curr>next ) {
+                maxdis=curridx-firstidx;
+                if((curridx-previdx)<mindis) mindis=curridx-previdx;
+                previdx=curridx;
+               
+            }
+            prev=curr;
+        }
+
+        return new int[]{mindis,maxdis};
+
+    }
+}
