e25cf3fde2951923f3634372ce38b6cbaca1950c
metarya <metaryajain07@gmail.com>
2026-09-02 11:41:21 +0530

    [LeetCode #3875] Construct Uniform Parity Array I - Solution 1 (Java)

---
+++ problems/3875-construct-uniform-parity-array-i/solution_1.java
@@ -0,0 +1,17 @@
+/*
+ * LeetCode Problem #3875: Construct Uniform Parity Array I
+ * URL: https://leetcode.com/problems/construct-uniform-parity-array-i/
+ * Solution #1 (Java)
+ * Status: Accepted
+ * Runtime: N/A
+ * Memory: 45288000
+ * Submission Date: 2026-09-02 06:11:22 UTC
+ * Submission ID: 2128067971
+ */
+
+class Solution {
+
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
