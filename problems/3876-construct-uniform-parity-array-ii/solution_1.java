21ce6f0200716e04ea98d636a574022492a918bd
metarya <metaryajain07@gmail.com>
2026-09-03 07:06:23 +0530

    [LeetCode #3876] Construct Uniform Parity Array II - Solution 1 (Java)

---
+++ problems/3876-construct-uniform-parity-array-ii/solution_1.java
@@ -0,0 +1,31 @@
+/*
+ * LeetCode Problem #3876: Construct Uniform Parity Array II
+ * URL: https://leetcode.com/problems/construct-uniform-parity-array-ii/
+ * Solution #1 (Java)
+ * Status: Accepted
+ * Runtime: 2
+ * Memory: 121896000
+ * Submission Date: 2026-09-03 01:36:23 UTC
+ * Submission ID: 2129053346
+ */
+
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int min=Integer.MAX_VALUE;
+        for(int num:nums1){
+            min=Math.min(min,num);
+        }
+
+        //minimum is odd->always possible
+        if(min%2==1){
+            return true;
+        }
+        //minimum is even -> all elements must be even 
+        for(int num:nums1){
+            if(num%2==1){
+                return false;
+            }
+        }
+        return true;
+    }
+}
